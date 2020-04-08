fc1ef68a9b7fcf8be3a64c76fc8e1d2aacb0b9cf
hritvik <hritvik12@gmail.com>
2020-04-08 21:57:17 +0530

    ejs rendered

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const port = 8000;
-
 const app = express();
+const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req, res){
-    res.send("It is running!");
+    return res.render('home');
 });
 app.get('/profile', function(req, res){
     res.send("Profile not found");
