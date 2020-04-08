3d0ad75e498b95ed501c81c9708b8f7f50f0dc96
hritvik <hritvik12@gmail.com>
2020-04-08 21:37:07 +0530

    express added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-hello world;
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+
+app.get('/', function(req, res){
+    res.send("It is running!");
+});
+app.get('/profile', function(req, res){
+    res.send("Profile not found");
+});
+
+
+
+app.listen(port, function(err){
+    if (err) {
+        console.log("Error in running the server", err);
+    }
+    console.log('Yup!My Server is running on Port', port);
+})
